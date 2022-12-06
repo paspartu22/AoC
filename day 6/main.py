75a5e8163b4327240064143b288da2308b243a01
paspartu <andreyovodov@yandex.ru>
2022-12-06 10:18:23 +0300

    day 6 one line

--- day 6/main.py
+++ day 6/main.py
@@ -1,10 +1,13 @@
 seq_len = 14
+ 
+print (min([i if len(set(open("data.txt").readline()[i-seq_len :i])) == seq_len else len(open("data.txt").readline()) 
+                                                        for i in range(seq_len-1, len(open("data.txt").readline()))]))
 
+''' part 1 / 2
 with open("data.txt") as file:
     for line in file:
-    #line = file.readline()
-        print(line)
         for i in range(seq_len-1, len(line)):
             if len(set(line[i-seq_len :i])) == seq_len:    
                 print (i)
                 break
+'''
