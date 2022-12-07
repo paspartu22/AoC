eceb8a2b8c178741bf7895f49cf02cee57c83b52
Paspartu <a@ovdv.ru>
2022-12-07 11:14:32 +0300

    day 7 part 2

--- day 7/main.py
+++ day 7/main.py
@@ -23,8 +23,18 @@ with open("data.txt", "r") as file:
                 dirs[dir] += int(line.split()[0])
                 
     print(dirs)
+    ''' part 1
     output = 0
     for weight in dirs.values():
         if weight <= 100000:
             output += weight
     print (output)
+    '''
+    # part 2
+    space_need = abs(70000000 - 30000000 - dirs["/"])
+    print(space_need)
+    min_dir = "/"
+    for dir in dirs:
+        if space_need < dirs[dir] < dirs[min_dir]:
+            min_dir = dir
+    print (dirs[min_dir])
