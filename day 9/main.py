a9e43d55a7cbd55255f55222f31d7ac2e29b07ef
paspartu <andreyovodov@yandex.ru>
2022-12-09 11:14:19 +0300

    day 9 part 2

--- day 9/main.py
+++ day 9/main.py
@@ -13,22 +13,18 @@ class Pos:
                 self.x += 1
             case("L"):
                 self.x -= 1
+    def __str__(self) -> str:
+        return f"{self.x} {self.y}"
 
 class Rope:
     def __init__(self):
-        knots=[]
+        self.knots=[]
         for i in range(10):
-            knots.append(Pos())
-        
-  
-
-head = Pos()
-tail = Pos()
-
-def move_tail():
-    dist = abs(head.x - tail.x) + abs(head.y - tail.y)
-    match(dist):
-        case(2):
+            self.knots.append(Pos())
+            
+    def move_tail(self, head, tail):
+        dist = abs(head.x - tail.x) + abs(head.y - tail.y)
+        if dist == 2:
             if head.x == tail.x:
                 if head.y > tail.y:
                     tail.move("U")
@@ -39,7 +35,7 @@ def move_tail():
                     tail.move("R")
                 else:
                     tail.move("L")
-        case(3):
+        elif dist >= 3: 
             if head.x > tail.x:
                 tail.move("R")
             else:
@@ -49,17 +45,28 @@ def move_tail():
             else:
                 tail.move("D")
 
+    def move_rope(self):
+        for i in range(1, 10):
+            self.move_tail(self.knots[i-1],self.knots[i])
+            #print (f"Knot {i} move to {self.knots[i]}")
+
+
+        
+rope = Rope()  
+
 visited = []
 
 with open ("data.txt", "r") as file:
     for line in file:
-        print (line)
         for i in range(int(line.split(" ")[1])):
-            head.move(line.split(" ")[0])
-            print (f"HEAD {head.x} {head.y}")
-            move_tail()
-            print (f"TAIL {tail.x} {tail.y}")
-            if [tail.x, tail.y] not in visited:
-                visited.append([tail.x, tail.y])
+            #print (line)
+            rope.knots[0].move(line.split(" ")[0])
+            print (f"HEAD {rope.knots[0]}")
+            
+            rope.move_rope()
+            print (f"TAIL {rope.knots[-1]}")
+
+            if [rope.knots[-1].x, rope.knots[-1].y] not in visited:
+                visited.append([rope.knots[-1].x, rope.knots[-1].y])
 
     print (len(visited))
