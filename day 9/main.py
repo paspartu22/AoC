1a51021346f15eef5cb5fee01f8dddaa33734526
Paspartu <a@ovdv.ru>
2022-12-09 13:26:49 +0300

    day 9 another one

--- day 9/main.py
+++ day 9/main.py
@@ -7,44 +7,47 @@ class Pos:
 
     def move(self, dir): 
         match(dir):
-            case("U"):
-                self.y += 1
-            case("D"):
-                self.y -= 1
-            case("R"):
-                self.x += 1
-            case("L"):
-                self.x -= 1
+            case("U"): self.y += 1
+            case("D"): self.y -= 1
+            case("R"): self.x += 1
+            case("L"): self.x -= 1
 
 class Rope:
-    def __init__(self): #create 10 knot rope
+    def __init__(self, lenght): #create 10 knot rope
         self.knots=[]
-        for i in range(10):
+        for i in range(lenght):
             self.knots.append(Pos())
             
-    def move_tail(self, head, tail): #move tail knot after head knot 
+    def move_next(self, head, tail): #move tail knot after head knot 
         if abs(head.x - tail.x) > 1 or abs(head.y - tail.y) > 1:
             tail.x += np.sign(head.x - tail.x)
             tail.y += np.sign(head.y - tail.y)
+            return True
+        else:
+            return False
             
     def move_rope(self): # move every knot from 1 to 9 (not the head)
-        for i in range(1, 10):
-            self.move_tail(self.knots[i-1],self.knots[i])
+        for i in range(1, len(self.knots)):
+            if not self.move_next(self.knots[i-1],self.knots[i]):
+                break
     
-rope = Rope()  
+rope = Rope(10)
 visited = [] #visited cells by tail
+
 
 def check_visited():
     if [rope.knots[-1].x, rope.knots[-1].y] not in visited:
         visited.append([rope.knots[-1].x, rope.knots[-1].y])
 
 def main ():   
+    last_move = [0,0]
     with open ("data.txt", "r") as file:
         for line in file:
             for i in range(int(line.split(" ")[1])):
                 rope.knots[0].move(line.split(" ")[0])
-                rope.move_rope()
+                rope.move_rope()                
                 check_visited()
+                
         print (len(visited))
 
 main()
