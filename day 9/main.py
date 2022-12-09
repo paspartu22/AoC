b6c9e011343fe35875fb776f334e8caffc2c7605
paspartu22 <andreyovodov@yandex.ru>
2022-12-09 11:55:35 +0300

    day 9 minimal

--- day 9/main.py
+++ day 9/main.py
@@ -5,7 +5,7 @@ class Pos:
         self.x = x
         self.y = y
 
-    def move(self, dir):
+    def move(self, dir): 
         match(dir):
             case("U"):
                 self.y += 1
@@ -15,42 +15,36 @@ class Pos:
                 self.x += 1
             case("L"):
                 self.x -= 1
-                
-    def __str__(self) -> str:
-        return f"{self.x} {self.y}"
 
 class Rope:
-    def __init__(self):
+    def __init__(self): #create 10 knot rope
         self.knots=[]
         for i in range(10):
             self.knots.append(Pos())
             
-    def move_tail(self, head, tail):
+    def move_tail(self, head, tail): #move tail knot after head knot 
         if abs(head.x - tail.x) > 1 or abs(head.y - tail.y) > 1:
             tail.x += np.sign(head.x - tail.x)
             tail.y += np.sign(head.y - tail.y)
             
-    def move_rope(self):
+    def move_rope(self): # move every knot from 1 to 9 (not the head)
         for i in range(1, 10):
             self.move_tail(self.knots[i-1],self.knots[i])
-
-
+    
 rope = Rope()  
-
 visited = [] #visited cells by tail
 
-with open ("data.txt", "r") as file:
-    for line in file:
-        for i in range(int(line.split(" ")[1])):
-            print()
-            print (line)
-            rope.knots[0].move(line.split(" ")[0])
-            print (f"HEAD {rope.knots[0]}")
-            
-            rope.move_rope()
-            print (f"TAIL {rope.knots[-1]}")
+def check_visited():
+    if [rope.knots[-1].x, rope.knots[-1].y] not in visited:
+        visited.append([rope.knots[-1].x, rope.knots[-1].y])
 
-            if [rope.knots[-1].x, rope.knots[-1].y] not in visited:
-                visited.append([rope.knots[-1].x, rope.knots[-1].y])
+def main ():   
+    with open ("data.txt", "r") as file:
+        for line in file:
+            for i in range(int(line.split(" ")[1])):
+                rope.knots[0].move(line.split(" ")[0])
+                rope.move_rope()
+                check_visited()
+        print (len(visited))
 
-    print (len(visited))
+main()
